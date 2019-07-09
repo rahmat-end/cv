81b219cd043db7ee5dd1ff8494f0559909e3cba3
Rahmat Kurniawan <rahmatkurniawan689@gmail.com>
2019-07-08 23:54:37 -0400

    baru

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        $("#p1").mouseenter(function () {
+            alert("You entered p1!");
+            // $("div").addClass("important");
+        });
+    });
+
+})(jQuery);
+
