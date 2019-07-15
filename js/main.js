aa62d1b1999e48b04539965218fbca66b558879a
Rahmat Kurniawan <rahmatkurniawan689@gmail.com>
2019-07-15 04:51:43 -0400

    fix lah

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("nav.menu > ul > li > a").click(function (event) {
         event.preventDefault()
 
-        $.each($("nav.menu > ul > li > a"), function(i, v){ 
+        $.each($("nav.menu > ul > li > a"), function (i, v) {
             $(this).removeClass("active")
         })
         $(this).addClass("active")
@@ -11,10 +11,50 @@ $(document).ready(function () {
 
         $("html").animate({
             scrollTop: $(hash).offset().top
-        }, 1000, function(){
+        }, 1000, function () {
             window.location.hash = hash
         })
     });
 
+    $("#triggerOpen").click(function () {
+        $("#myNav").css("width", "100%");
+        $("body").css("overflow", "hidden");
+    });
+
+    $("#triggerClose").click(function () {
+        $("#myNav").css("width", "0%");
+        $("body").css("overflow", "visible");
+    });
+
+    $("#myNav .overlay-content a").click(function (event) {
+        event.preventDefault()
+
+        $("#myNav").css("width", "0%");
+        $("body").css("overflow", "visible");
+
+        $.each($("#myNav .overlay-content a"), function (i, v) {
+            $(this).removeClass("active")
+        })
+        $(this).addClass("active")
+
+        var hash = this.hash
+
+        $("html").animate({
+            scrollTop: $(hash).offset().top
+        }, 2000, function () {
+            window.location.hash = hash
+        })
+    });
+
+    $("nav.menu > ul > li > a").click(function (event) {
+        event.preventDefault()
+
+        $("#myNav").css("width", "0%");
+        $("body").css("overflow", "visible");
+    });
+
+    // $("@media (max-width: 991px)").onload(function () {
+    //     $("body").removeClass("nav.menu");
+    // });
 
 });
